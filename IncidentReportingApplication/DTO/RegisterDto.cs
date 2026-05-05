9ec4508494a32791b3fe8d9530f48482917f3771
Oregbemi Afolabi <oregbemiafolabi@Oregbemis-MacBook-Pro.local>
2026-05-05 03:09:40 +0100

    revert to docker

--- IncidentReportingApplication/DTO/RegisterDto.cs
+++ IncidentReportingApplication/DTO/RegisterDto.cs
@@ -2,8 +2,8 @@
 {
     public class RegisterDto
     {
-        public string Email { get; set; }
-        public string FullName { get; set; }
-        public string Password { get; set; }
+        public required string Email { get; set; }
+        public required string FullName { get; set; }
+        public required string Password { get; set; }
     }
 }
